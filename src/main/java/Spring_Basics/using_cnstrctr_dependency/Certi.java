2a254c0065d90cc0a618c8cae48fcb46ad144b66
ManeeshaChandel <maneeshachandel27@gmail.com>
2023-04-28 21:35:49 +0530

    Adding spring examples

--- src/main/java/Spring_Basics/using_cnstrctr_dependency/Certi.java
+++ src/main/java/Spring_Basics/using_cnstrctr_dependency/Certi.java
@@ -1,4 +1,4 @@
-package using_cnstrctr_dependency;
+package Spring_Basics.using_cnstrctr_dependency;
 // I have made this class ,
 //- by passing parameter in constructor of Person class
 // click on the type "Certi" (in parameter)
